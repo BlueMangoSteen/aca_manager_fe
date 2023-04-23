c4eeafb8ace34dadb957c223e887e436d1b77119
DaeHyun_Woo <po668312@naver.com>
2023-04-23 23:59:11 +0900

    setting: 프로젝트 보일러 플레이팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ACA - 학원 관리 시스템</title>
+      </Head>
+      <main></main>
+    </>
+  );
+}
